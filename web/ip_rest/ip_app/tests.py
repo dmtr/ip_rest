ebfd29e24a08d0eef1cfa30b14b9680d5858cb16
Dmitriy Sorochenkov <podotkos@gmail.com>
2016-07-13 16:33:57 +0300

    Add tests

--- web/ip_rest/ip_app/tests.py
+++ web/ip_rest/ip_app/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from ip_app.models import IpLogModel
 
-# Create your tests here.
+
+class IplogTests(APITestCase):
+
+    def test_create_log_record(self):
+        url = reverse('ip-list')
+        response = self.client.post(url, {}, format='json', HTTP_X_REAL_IP='127.0.0.1')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(IpLogModel.objects.count(), 1)
+        self.assertEqual(IpLogModel.objects.get().ip, '127.0.0.1')
+
+    def test_create_log_record_fail(self):
+        url = reverse('ip-list')
+        response = self.client.post(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(IpLogModel.objects.count(), 0)
+
+    def test_get_list(self):
+        url = reverse('ip-list')
+        self.client.post(url, {}, format='json', HTTP_X_REAL_IP='127.0.0.1')
+        self.client.post(url, {}, format='json', HTTP_X_REAL_IP='172.10.0.2')
+        self.assertEqual(IpLogModel.objects.count(), 2)
+        response = self.client.get(url)
+        self.assertEqual(2, len(response.data))
+        self.assertEqual('127.0.0.1', response.data[0]['ip'])
+        response = self.client.get(url + '?limit=1&offset=1')
+        self.assertEqual('172.10.0.2', response.data['results'][0]['ip'])
--- web/ip_rest/ip_app/urls.py
+++ web/ip_rest/ip_app/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^ip/$', views.IpList.as_view()),
+    url(r'^ip/$', views.IpList.as_view(), name='ip-list'),
 ]
