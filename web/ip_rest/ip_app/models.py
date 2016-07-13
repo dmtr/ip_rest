1325bdb60a8a7826c708a7cfdb026af811f782ff
Dmitriy Sorochenkov <podotkos@gmail.com>
2016-07-13 15:16:57 +0300

    Add timestamp auto creation; Add migrations

--- web/ip_rest/ip_app/models.py
+++ web/ip_rest/ip_app/models.py
@@ -5,7 +5,7 @@ class IpLogModel(models.Model):
 
     ip = models.GenericIPAddressField('Ip address')
 
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return 'ip: {o.ip}, timestamp: {o.timestamp}'.format(o=self)
