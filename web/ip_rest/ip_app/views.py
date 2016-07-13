4784d3382d0962a56fa33a0d5a54dd94320044fa
Dmitriy Sorochenkov <podotkos@gmail.com>
2016-07-13 15:57:12 +0300

    Use Generic View for ip list

--- web/ip_rest/ip_app/views.py
+++ web/ip_rest/ip_app/views.py
@@ -1,37 +1,27 @@
 import logging
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
+
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
+
 from ip_app.models import IpLogModel
 from ip_app.serializers import IpLogSerializer
 
 logger = logging.getLogger(__name__)
 
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+class IpList(generics.ListCreateAPIView):
+    queryset = IpLogModel.objects.all()
+    serializer_class = IpLogSerializer
+    pagination_class = LimitOffsetPagination
 
-
-@csrf_exempt
-def ip_list(request):
-    """
-    List all log records, or create a new one.
-    """
-    if request.method == 'GET':
-        records = IpLogModel.objects.all()
-        serializer = IpLogSerializer(records, many=True)
-        return JSONResponse(serializer.data)
-    elif request.method == 'POST':
-        ip = request.META['HTTP_X_REAL_IP']
+    def post(self, request):
+        ip = request.META.get('HTTP_X_REAL_IP')
         logger.debug('ip: %s', ip)
         serializer = IpLogSerializer(data={'ip': ip})
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        logger.error('Real ip not found in headers')
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
